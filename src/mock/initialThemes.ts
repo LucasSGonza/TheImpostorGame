2fcab9cfcb48f892f26cd44f8fdf0fb4d2823507
LucasSGonza <lucassilva.goncalves@hotmail.com>
2025-12-02 15:51:51 -0300

    feat: adiciona base logica e visual do projeto

---
+++ src/mock/initialThemes.ts
@@ -0,0 +1,24 @@
+export const initialThemes = [
+  {
+    id: "1",
+    name: "Esportes",
+    words: ["Futebol", "Basquete", "Vôlei", "Natação", "Tênis", "Boxe"],
+  },
+  {
+    id: "2",
+    name: "Filmes",
+    words: [
+      "Titanic",
+      "Avatar",
+      "Matrix",
+      "Interestelar",
+      "Coringa",
+      "Parasita",
+    ],
+  },
+  {
+    id: "3",
+    name: "Países",
+    words: ["Brasil", "Japão", "França", "Egito", "Canadá", "Austrália"],
+  },
+];
